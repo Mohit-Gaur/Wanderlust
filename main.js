73657a26a4d01cd274afad232bd44920eb95f01d
Mohit Gaur <56885276+Mohit-Gaur@users.noreply.github.com>
2020-06-29 16:42:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,62 +13,54 @@ const $submit = $('#button');
 const $destination = $('#destination');
 const $container = $('.container');
 const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
-const $weatherDiv = $("#weather1");
+const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4")];
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 // Add AJAX functions here:
-const getVenues = async () => {
-  const city = $input.val();
-  const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20180101`;
+const getVenues = async() => {
+	const city = $input.val();
+  const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20190202`;
   try {
     const response = await fetch(urlToFetch);
-    if(response.ok)
-    {
-      console.log(response);
+    if(response.ok) {
       const jsonResponse = await response.json();
-      console.log(jsonResponse);
       const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
-      console.log(venues)
       return venues;
     }
-    else
-    {
-      throw new Error('Request Failed');
+    else {
+      throw new Error('Request failed!');
     }
-} 
-catch (error) {
-  console.log(error.message);  
+  }
+  catch(error) {
+    console.log(error.message);
+  }
 }
-};
 
-const getForecast = async () => {
-  const  urlToFetch = `${forecastUrl}${apikey}&q = ${$input.val()}&days=4&hour=11`;
-  try{
-    const response = await fetch(utlToFetch);
-    if(response.ok){
+const getForecast = async() => {
+  const urlToFetch = `${forecastUrl}${apiKey}&q=${$input.val()}&days=4&hour=11`;
+	try {
+    const response = await fetch(urlToFetch);
+    if(response.ok) {
       const jsonResponse = await response.json();
-      console.log(jsonResponse);
       const days = jsonResponse.forecast.forecastday;
       return days;
     }
-    else{
-      throw new Error('Request Failed')
+    else {
+      throw new Error('Request failed!');
     }
   }
-  catch(error){
+  catch(error) {
     console.log(error.message);
   }
-
 }
 
 
 // Render functions
 const renderVenues = (venues) => {
   $venueDivs.forEach(($venue, index) => {
-    // Add your code here:
-    const venue = venues.[index]
-    const venueIcon = venue.categories[0].icon
-    const venueImgsrc = `${venueIcon.prefix}bg_64${venueIcon.suffix}`;
+    const venue = venues[index];
+    const venueIcon = venue.categories[0].icon;
+    const venueImgSrc = `${venueIcon.prefix}bg_64${venueIcon.suffix}`;
     let venueContent = createVenueHTML(venue.name, venue.location, venueImgSrc);
     $venue.append(venueContent);
   });
@@ -76,20 +68,20 @@ const renderVenues = (venues) => {
 }
 
 const renderForecast = (days) => {
-  $weatherDivs.forEach($day, index) => {
+  $weatherDivs.forEach(($day, index) => {
     const currentDay = days[index];
     let weatherContent = createWeatherHTML(currentDay);
     $day.append(weatherContent);
-  }
+  });
 }
 
 const executeSearch = () => {
   $venueDivs.forEach(venue => venue.empty());
-  $weatherDiv.empty();
+  $weatherDivs.forEach(day => day.empty());
   $destination.empty();
   $container.css("visibility", "visible");
-  getVenues().then(venues => renderVenues(venues));
-  getForecast().then(forecasat => renderForecast(forecast));
+  getVenues().then(venues => renderVenues(venues))
+  getForecast().then(forecast => renderForecast(forecast));
   return false;
 }
 
